88063ea2678021316695345dbc3045cdec92dbdb
peixot_a <pierre.peixoto@epitech.eu>
2016-06-04 16:13:35 +0200

    pushing final version

--- src/body/leav.c
+++ src/body/leav.c
@@ -5,7 +5,7 @@
 ** Login   <lallia_m@epitech.net>
 **
 ** Started on  Thu Jun  2 22:36:02 2016 Marc Lallias
-** Last update Sat Jun  4 15:57:34 2016 
+** Last update Sat Jun  4 16:12:26 2016 
 */
 
 #include "../../inc/minishell2.h"
@@ -15,7 +15,10 @@ int	leav_shell(t_int *l_pid, t_put *curr)
   while (l_pid)
     {
       if (l_pid->data_int > 0)
-	kill(l_pid->data_int, SIGINT);
+	{
+	  if (kill(l_pid->data_int, SIGINT) == -1)
+	    return (-1);
+	}
       l_pid = l_pid->next;
     }
   curr->exit_stat = NEED_EXIT;
